62a74974793a222654d779ff4d2a85cae41edcc7
Vaskevych-O-O <oleh.vaskevych.ki.2022@lpnu.ua>
2024-11-19 14:00:47 +0200

    INIT: Lab2 initial commit

---
+++ Lab2/javaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"ki305.vaskevych.lab2","l":"Breed"},{"p":"ki305.vaskevych.lab2","l":"Dog"},{"p":"ki305.vaskevych.lab2","l":"DogTag"},{"p":"ki305.vaskevych.lab2","l":"Main"},{"p":"ki305.vaskevych.lab2","l":"Owner"}];updateSearchResults();
